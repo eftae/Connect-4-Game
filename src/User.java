c6d5b7fa688f01868f1082f2185c0df7309b00fb
WAI <w_yeung@hotmail.com>
2015-05-04 12:06:13 +1000

    User v0.1
    v0.1: allows for command line input only.

--- src/User.java
+++ src/User.java
@@ -1,4 +1,33 @@
+/**
+ * Handle user input
+ * v0.1: allows for command line input only.
+ * 
+ * @version v0.1
+ */
 
-public class User extends Player{
+import java.util.Scanner;
 
+public class User implements Player {
+
+	public int decideMove(GameState currState) {
+		return modeCommandLineInput(currState);
+	}
+
+	/**
+	 * get next move from user in command line
+	 * 
+	 * @param currState
+	 * @return 0 - 6
+	 */
+	public int modeCommandLineInput(GameState currState) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Next move (1-7): ");
+		int nextMove = sc.nextInt();
+		while (currState.isColFull(nextMove)) {
+			System.out.print("Invalid move, try again (1-7): ");
+			nextMove = sc.nextInt();
+		}
+		sc.close();
+		return nextMove - 1;
+	}
 }
