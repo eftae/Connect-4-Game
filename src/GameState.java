f519f97fcd48bf94a461061323376f50d6f92b93
Kelvin Li <kelvinlcf@yahoo.com.hk>
2015-05-04 11:20:25 +1000

    added board and players to gamestate

--- src/GameState.java
+++ src/GameState.java
@@ -1,4 +1,18 @@
 
 public class GameState {
+    private final int COL_MAX = 7;
+    private final int ROW_MAX = 6;
+    
+    private Disc[COL_MAX][ROW_MAX] board;
+    private ArrayList<Player> players;
+    
+    
+    public GameState() {
+        // initialise the board to null
+        for(int i = 0; i < COL_MAX; i++)
+            for(int j = 0; j < ROW_MAX; j++)
+                board[i][j] = null;
 
+        players = new ArrayList<Player>();
+    }
 }
