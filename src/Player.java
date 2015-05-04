3c32228f11670f263996afb643ae66c0b9a2ddd1
WAI <w_yeung@hotmail.com>
2015-05-04 11:21:47 +1000

    Player v0.1

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,10 @@
+/**
+ * Interface for Player. Player includes User, AI
+ * 
+ * @version v0.1
+ *
+ */
 
-public class Player {
-
+public interface Player {
+	public int decideMove(GameState currState);
 }
