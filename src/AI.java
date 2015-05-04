f2c3f0439d64045cbefd3d734751baa5870ac9e1
WAI <w_yeung@hotmail.com>
2015-05-04 11:19:11 +1000

    AI v0.1
    mode 0: fully randomized

--- src/AI.java
+++ src/AI.java
@@ -1,4 +1,45 @@
+/**
+ * Class for the AI. Mode: 0. Fully randomized
+ * 
+ * @version v0.1
+ */
 
-public class AI extends Player{
+import java.util.Random;
+
+public class AI implements Player {
+	private int mode;
+
+	/**
+	 *
+	 * @param mode
+	 *            AI mode
+	 */
+	public AI(int mode) {
+		this.mode = mode;
+	}
+
+	public int decideMove(GameState currState) {
+
+		switch (mode) {
+		default:
+			return modeRandom(currState);
+		}
+
+	}
+
+	/**
+	 * AI mode in fully randomization
+	 * 
+	 * @param currState
+	 * @return next column move
+	 */
+	public int modeRandom(GameState currState) {
+		Random rand = new Random();
+		int nextMove = rand.nextInt(7);
+		while (!currState.isColFull(nextMove, currState)) {
+			nextMove = rand.nextInt(7);
+		}
+		return nextMove;
+	}
 
 }
