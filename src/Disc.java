a49cad090459e12e3eb47f576528d3f78fa39671
chrischchan <chrischchan@users.noreply.github.com>
2015-05-06 10:57:15 +1000

    Update Disc.java
    Disc contains its own co-ordinates for easier data management.
    Assimilated this version so constructor and return are the same.

--- src/Disc.java
+++ src/Disc.java
@@ -1,17 +1,36 @@
 /**
  * Class for a disc.
  * 
- * @version v0.1
+ * @version v0.2
  */
 
 public class Disc {
-	Player affiliation; // belong to a player
-
-	public Disc(Player affiliation) {
-		this.affiliation = affiliation;
+	private Player currPlayer;
+	private int x;
+	private int y;
+	
+	public Disc(Player p) {
+		currPlayer = p;
 	}
-
+	
+	public void setPlayer(Player p) {
+		this.currPlayer = p;
+	}
+	
 	public Player getAffiliation() {
-		return affiliation;
+		return this.currPlayer;
+	}
+	
+	public int getHoriz() {
+		return this.x;
+	}
+	
+	public int getVert() {
+		return this.y;
+	}
+	
+	public void setLocation(int c, int r) {
+		this.x = c;
+		this.y = r;
 	}
 }
